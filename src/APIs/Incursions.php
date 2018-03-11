434032714b595d4030ef86e072b093a07b69a017
michaelcooke <michael.frank.cooke@gmail.com>
2018-03-11 19:29:41 -0400

    Implement incursions endpoint

---
+++ src/APIs/Incursions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MichaelCooke\Orthrus\Apis;
+
+use MichaelCooke\Orthrus\Orthrus;
+use MichaelCooke\Orthrus\Apis\Api;
+
+class Incursions extends Api
+{
+    public function __construct(Orthrus $orthrus)
+    {
+        $this->base = "incursions";
+        $this->orthrus = $orthrus;
+    }
+
+    protected function get()
+    {
+        $this->verb = "get";
+    }
+}
