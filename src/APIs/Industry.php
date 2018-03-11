10b85bfd64cc7d2078ba1a492cf04c5762da3444
michaelcooke <michael.frank.cooke@gmail.com>
2018-03-11 19:32:24 -0400

    Implement industry endpoints

---
+++ src/APIs/Industry.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MichaelCooke\Orthrus\Apis;
+
+use MichaelCooke\Orthrus\Orthrus;
+use MichaelCooke\Orthrus\Apis\Api;
+
+class Industry extends Api
+{
+    public function __construct(Orthrus $orthrus)
+    {
+        $this->base = "industry";
+        $this->orthrus = $orthrus;
+    }
+
+    protected function getFacilities()
+    {
+        $this->verb = "get";
+        $this->endpoint = "facilities";
+    }
+
+    protected function getCostIndices()
+    {
+        $this->verb = "get";
+        $this->endpoint = "systems";
+    }
+}
