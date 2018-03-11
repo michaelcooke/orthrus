83aa1c96f156fc5fa5b45e2212d9128e2641598f
michaelcooke <michael.frank.cooke@gmail.com>
2018-03-11 19:34:47 -0400

    Implement insurance endpoint

---
+++ src/APIs/Insurance.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MichaelCooke\Orthrus\Apis;
+
+use MichaelCooke\Orthrus\Orthrus;
+use MichaelCooke\Orthrus\Apis\Api;
+
+class Insurance extends Api
+{
+    public function __construct(Orthrus $orthrus)
+    {
+        $this->base = "insurance";
+        $this->orthrus = $orthrus;
+    }
+
+    protected function getPrices()
+    {
+        $this->verb = "get";
+        $this->endpoint = "prices";
+    }
+}
