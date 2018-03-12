f261c3ec9966ef18f5451d03dc769cde659bad2f
Michael Cooke <michael.frank.cooke@gmail.com>
2018-03-12 12:58:13 -0400

    Implement UI endpoints

---
+++ src/APIs/UI.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MichaelCooke\Orthrus\Apis;
+
+use MichaelCooke\Orthrus\Orthrus;
+use MichaelCooke\Orthrus\Apis\Api;
+
+class Wars extends Api
+{
+    public function __construct(Orthrus $orthrus)
+    {
+        $this->base = 'ui';
+        $this->verb = 'post';
+        $this->orthrus = $orthrus;
+    }
+
+    protected function marketDetails(Int $typeId)
+    {
+        $this->query = ['type_id' => $typeId];
+        $this->endpoint = 'openwindow/marketdetails'
+    }
+
+    protected function contract(Int $contractId)
+    {
+        $this->query = ['contract_id' => $contractId];
+        $this->endpoint = 'openwindow/marketdetails'
+    }
+
+    protected function information(Int $typeId)
+    {
+        $this->query = ['target_id' => $typeId];
+        $this->endpoint = 'openwindow/marketdetails'
+    }
+
+    protected function autopilot(Int $systemId, Bool $addToBeginning = false, Bool $clearWaypoints = false)
+    {
+        $this->query = [
+            'destination_id' => $systemId,
+            'add_to_beginning' => $addToBeginning,
+            'clear_other_waypoints' => $clearWaypoints
+        ];
+        $this->endpoint = 'autopilot/waypoint';
+    }
+}
