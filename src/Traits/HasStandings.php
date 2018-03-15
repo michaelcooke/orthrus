65563247b528d7a20478306a8dd5bb6ecf11cd67
Michael Cooke <michael.frank.cooke@gmail.com>
2018-03-15 17:56:02 -0400

    Add paging logic in HasAssets and HasStandings

--- src/Traits/HasStandings.php
+++ src/Traits/HasStandings.php
@@ -4,8 +4,13 @@ namespace MichaelCooke\Orthrus\Traits;
 
 trait HasStandings
 {
-    protected function standings(): void
+    protected function standings(int $page = null): void
     {
+        $this->query = ['page' => $page];
         $this->endpoint = $this->id . '/standings';
+
+        if ($page == null) {
+            $this->getAllPages = true;
+        }
     }
 }
